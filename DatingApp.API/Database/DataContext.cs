3576ae71523b8405673424757480692f75b82562
Tùng Hoàng <minhtungg2504@gmail.com>
2021-09-18 19:12:35 +0700

    Migration Database

---
+++ DatingApp.API/Database/DataContext.cs
@@ -0,0 +1,10 @@
+using DatingApp.DatingApp.API.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+namespace DatingApp.DatingApp.API.Database
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ DatingApp.API/Database/Entities/User.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.DatingApp.API.Database.Entities
+{
+    [Table("User")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(32)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(32)]
+        public string Email { get; set; }
+    }
+}
